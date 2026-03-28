0ce19f5a2cdcef950f6949bef6d707f45139a84e
Tunar Mammadov <87617543+tunbyte@users.noreply.github.com>
2026-03-28 17:08:24 +0300

    Longest Common Prefix #19 solution

---
+++ longestCommonPrefix#14.py
@@ -0,0 +1,33 @@
+"""
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        res = ""
+
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for word in strs:
+                if i >= len(word) or word[i] != char:
+                    return res
+            res += char
+        return res
+
+
+
+
+
+inp = [
+    'salam',
+    'meleyki'
+]
+
+sol = Solution().longestCommonPrefix(inp)
+print(sol)
